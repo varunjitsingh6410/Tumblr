811c27d438f1e9ce021f582335fed37f49f63a48
Varun Jit Singh <varun@administorsmbp2.attlocal.net>
2018-12-18 13:11:36 -0800

    Added navigation to photo detail vc

---
+++ Tumblr/PhotoDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailsViewController.swift
+//  Tumblr
+//
+//  Created by Varun Jit Singh on 12/18/18.
+//  Copyright © 2018 Varun Jit Singh. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
